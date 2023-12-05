ab882e1a2508ba5cb06c329fa98509a731742689
Alexandre Petitjean <alpetitjean@gmail.com>
2023-12-05 22:24:50 +0100

    Day 04

---
+++ day04/src/main.rs
@@ -0,0 +1,94 @@
+fn get_numbers(line: &str, start_idx: usize, end_idx: usize) -> Vec<i32> {
+    let mut result = vec![];
+
+    if let Some(sl) = line.get(start_idx..end_idx) {
+        for num_str in sl.split(' ') {
+            if let Ok(number) = num_str.trim().parse() {
+                result.push(number);
+            }
+        }
+    }
+
+    return result;
+}
+
+pub fn get_winning_count(line: &str) -> usize {
+    let mut count = 0;
+
+    let colon_idx = line.find(':').unwrap();
+    let sep_idx = line.find('|').unwrap();
+
+    let card_numbers = get_numbers(line, colon_idx, sep_idx);
+    let our_numbers = get_numbers(line, sep_idx, line.len());
+
+    for n in our_numbers {
+        if card_numbers.contains(&n) {
+            count += 1
+        }
+    }
+
+    return count;
+}
+
+pub fn part1(filename: &str) -> usize {
+    let mut sum = 0;
+    if let Ok(lines) = common::read_lines(filename) {
+        for line in lines {
+            if let Ok(card) = line {
+                let n = get_winning_count(card.as_str());
+                if n > 0 {
+                    sum += 2_usize.pow((n - 1) as u32);
+                }
+            }
+        }
+    }
+    return sum;
+}
+
+pub fn part2(filename: &str) -> usize {
+    let mut deck = vec![];
+
+    let mut card_idx: usize = 1;
+
+    if let Ok(lines) = common::read_lines(filename) {
+        for line in lines {
+            if let Ok(card) = line {
+
+                while deck.len() < card_idx {
+                    deck.push(1);
+                }
+
+                let n = get_winning_count(card.as_str());
+
+                for i in 1..n + 1 {
+                    if deck.len() < card_idx + i {
+                        deck.push(deck[card_idx - 1] + 1);
+                    } else {
+                        deck[card_idx - 1 + i] += deck[card_idx - 1];
+                    }
+                }
+
+                card_idx += 1;
+            }
+        }
+    }
+
+    return deck.iter().sum();
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() { assert_eq!(part1("test_day04.txt"), 13); }
+
+    #[test]
+    fn test_part2() { assert_eq!(part2("test_day04.txt"), 30); }
+}
+
+fn main() {
+    println!("Day04 Part1 = {}", part1("day04.txt"));
+    println!("Day04 Part2 = {}", part2("day04.txt"));
+}
