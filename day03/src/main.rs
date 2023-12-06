1026a7c1cb423275fbbfb18753c9d464834f0b76
Alexandre Petitjean <alpetitjean@gmail.com>
2023-12-06 21:24:17 +0100

    Day 03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,13 +1,10 @@
 use std::collections::HashMap;
-use std::ops::Deref;
 
 #[derive(Hash, Eq, PartialEq, Debug)]
 struct Position {
     line: i32,
     column: i32,
 }
-
-struct StarValues(i32, i32);
 
 fn get_next_number(line: &str, start_idx: usize) -> (Option<&str>, usize, usize) {
     if let Some(sl) = line.get(start_idx..) {
@@ -72,45 +69,45 @@ fn check_line(current: &str, previous: &str, next: &str) -> Vec<i32>
     return result;
 }
 
-fn insert_or_get(line: i32, column: i32, stars: &mut HashMap<Position, StarValues>) -> &StarValues {
-    let position = Position { line, column };
-    let v = stars.get(&position);
-
-    match v {
-        None => {
-            let values = StarValues(0, 0);
-            return *stars.insert(position, values);
-        }
-        Some(_) => { v }
-    }
-}
-
-fn check_stars(current: &str, previous: &str, next: &str, stars: &mut HashMap<Position, StarValues>, current_idx: i32) {
+fn check_stars(current: &str, previous: &str, next: &str, current_idx: i32, stars: &mut HashMap<Position, Vec<i32>>) {
     let (mut number, mut start_idx, mut end_idx) = get_next_number(current, 0);
 
     while let Some(n) = number {
-        let mut has_star = false;
         let mut extent_start_idx = 0;
         let mut star: Option<Position> = None;
 
         if start_idx > 0 {
-            if current.chars().nth(start_idx - 1).unwrap() == '*' { star = Some(Position { line: current_idx, column: (start_idx - 1) as i32 }); }
-            extent_start_idx = start_idx - 1;
+            if let Some(c) = current.chars().nth(start_idx - 1) {
+                if c == '*' {
+                    star = Some(Position { line: current_idx, column: (start_idx - 1) as i32 });
+                }
+                extent_start_idx = start_idx - 1;
+            }
         }
         if end_idx < current.len() - 1 {
-            if current.chars().nth(end_idx + 1).unwrap() == '*' { has_star = true; }
+            if let Some(c) = current.chars().nth(end_idx + 1) {
+                if c == '*' {
+                    star = Some(Position { line: current_idx, column: (end_idx + 1) as i32 });
+                }
+            }
+        }
+        if let Some(sub) = previous.get(extent_start_idx..std::cmp::min(end_idx + 2, previous.len())) {
+            if let Some(s) = sub.find('*') {
+                star = Some(Position { line: current_idx - 1, column: (extent_start_idx + s) as i32 });
+            }
+        }
+        if let Some(sub) = next.get(extent_start_idx..std::cmp::min(end_idx + 2, next.len())) {
+            if let Some(s) = sub.find('*') {
+                star = Some(Position { line: current_idx + 1, column: (extent_start_idx + s) as i32 });
+            }
         }
 
-        // for line in [previous, next] {
-        //     if !check_dot_only(line, extent_start_idx, std::cmp::min(end_idx + 2, line.len())) {
-        //         has_adjacent = true;
-        //     }
-        // }
-
-        if has_star {
-            // if let Ok(v) = n.parse::<i32>() {
-            //     result.push(v);
-            // }
+        if let Some(s) = star {
+            if let Some(sv) = stars.get_mut(&s) {
+                sv.push(n.parse().unwrap());
+            } else {
+                stars.insert(s, vec![n.parse().unwrap()]);
+            }
         }
 
         (number, start_idx, end_idx) = get_next_number(current, end_idx + 1);
@@ -150,21 +147,20 @@ pub fn part1(filename: &str) -> (Vec<i32>, i32) {
 
 pub fn part2(filename: &str) -> (Vec<i32>, i32) {
     let mut result = vec![];
+    let mut stars: HashMap<Position, Vec<i32>> = Default::default();
 
     if let Ok(lines) = common::read_lines(filename) {
         let mut current_line = "".to_string();
         let mut previous_line = "".to_string();
         let mut next_line: String;
-        let mut line_idx = 0;
-        let mut stars: HashMap<Position, StarValues>;
+        let mut line_idx = -1;
 
         for row in lines {
             if let Ok(line) = row {
                 next_line = line;
 
                 if !current_line.is_empty() {
-                    let mut line_res = check_stars(current_line.as_str(), previous_line.as_str(), next_line.as_str(), &stars, line_idx);
-                    result.append(&mut line_res);
+                    check_stars(current_line.as_str(), previous_line.as_str(), next_line.as_str(), line_idx, &mut stars);
                 }
 
                 previous_line = current_line;
@@ -173,9 +169,15 @@ pub fn part2(filename: &str) -> (Vec<i32>, i32) {
             line_idx += 1;
         }
 
-        let mut line_res = check_line(current_line.as_str(), previous_line.as_str(), "");
-        result.append(&mut line_res);
+        check_stars(current_line.as_str(), previous_line.as_str(), "", line_idx, &mut stars);
     }
+
+    for star in stars {
+        if star.1.len() == 2 {
+            result.push(star.1[0] * star.1[1]);
+        }
+    }
+
 
     let sum = result.iter().sum();
     (result, sum)
@@ -205,5 +207,5 @@ mod tests {
 
 fn main() {
     println!("Day03 Part1 = {}", part1("day03.txt").1);
-    // println!("Day03 Part2 = {}", part2("day03.txt").1);
+    println!("Day03 Part2 = {}", part2("day03.txt").1);
 }
